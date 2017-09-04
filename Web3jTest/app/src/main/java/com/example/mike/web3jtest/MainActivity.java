30e9e44070fe23c65cfda056550e70cc829888c3
MichalTurcel <michal.turcel@gmail.com>
2017-09-04 11:55:17 +0200

    Web3jTest initial commit

---
+++ Web3jTest/app/src/main/java/com/example/mike/web3jtest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mike.web3jtest;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.http.HttpService;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/uKNKPMg61M00bdiCyHgY"));  // defaults to http://localhost:8545/
+                try {
+                    return web3.web3ClientVersion().sendAsync().get().getWeb3ClientVersion();
+                } catch (InterruptedException | ExecutionException e) {
+                    Log.e("web3j version", "excep", e);
+                }
+                return "error";
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                Log.d("web3j version", s);
+            }
+        }.execute();
+
+
+    }
+}
